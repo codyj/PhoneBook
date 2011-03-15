db0b2d1fd4defacdb35b066c7af118b211f840a2
Cody Jorgensen <cody.d.jorgensen@gmail.com>
2011-03-15 01:52:41 -0700

    Initial Commit

---
+++ PhoneBookTests/PhoneBookTests.h
@@ -0,0 +1,17 @@
+//
+//  PhoneBookTests.h
+//  PhoneBookTests
+//
+//  Created by Cody Jorgensen on 3/15/11.
+//  Copyright 2011 Objective Coders. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface PhoneBookTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ PhoneBookTests/PhoneBookTests.m
@@ -0,0 +1,33 @@
+//
+//  PhoneBookTests.m
+//  PhoneBookTests
+//
+//  Created by Cody Jorgensen on 3/15/11.
+//  Copyright 2011 Objective Coders. All rights reserved.
+//
+
+#import "PhoneBookTests.h"
+
+
+@implementation PhoneBookTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PhoneBookTests");
+}
+
+@end
